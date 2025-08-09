ae674a8726e5c701b043d47a83acb707b3dcee13
Alex <puyaalexxx@gmail.com>
2025-08-09 13:29:15 +0300

    added an array of forecast data and displayt it in the template

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -13,7 +13,29 @@ class WeatherController extends AbstractController
     #[Route('/weather/{countryCode}/{city}', name: 'weather')]
     public function forecast(string $countryCode, string $city): Response{
 
+        $data = [
+            [
+              'tempaeratureCelsius' => 20,
+              'humidity' => 50,
+              'windSpeed' => 10,
+              'pressure' => 1012,
+            ],
+            [
+                'tempaeratureCelsius' => 22,
+                'humidity' => 55,
+                'windSpeed' => 12,
+                'pressure' => 1010,
+            ],
+            [
+                'tempaeratureCelsius' => 19,
+                'humidity' => 60,
+                'windSpeed' => 8,
+                'pressure' => 1015,
+            ],
+        ];
+
         return $this->render('weather/forecast.html.twig', [
+            'forecasts' => $data,
             'countryCode' => $countryCode,
             'city' => $city,
         ]);
